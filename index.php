3a1648d4d03cf454b68a1a7b77b51f93c707612f
Jorge Sidgo-Pimentel <jsidgopimentel21@outlook.com>
2018-03-16 00:26:40 -0600

    Estructura Inicial

--- index.php
+++ index.php
@@ -4,8 +4,16 @@
 <head>
 	<meta charset="UTF-8">
 	<title>SelectSalv</title>
-</head>
+	<link rel="icon" type="image/png" sizes="32x32" href="res/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="res/favicon/favicon-16x16.png">
+	<link rel="stylesheet" href="res/css/bootstrap.css">
+	<link rel="stylesheet" href="res/css/main.css">
+	<script src="res/js/jquery.js"></script>
+	<script src="res/js/bootstrap.min.js"></script>
 <body>
-	
+	<nav class="navbar"></nav>
+	<div id="hero-1">
+		
+	</div>
 </body>
 </html>
