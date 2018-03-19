11b624ff7cb74b20f4332b5023f75ac62762601d
Jorge Sidgo-Pimentel <jsidgopimentel21@outlook.com>
2018-03-18 22:02:39 -0600

    menusito

--- res/php/dashboard.php
+++ res/php/dashboard.php
@@ -11,6 +11,8 @@
 <head>
 	<meta charset="UTF-8">
 	<title><?php echo $nomUsuario; ?> | Dashboard</title>
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
 	<link rel="icon" type="image/png" sizes="32x32" href="../favicon/favicon-32x32.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="../favicon/favicon-16x16.png">
 	<link rel="stylesheet" href="../plugins/BootstrapMD/BootstrapMD.css">
@@ -28,7 +30,11 @@
 	<nav>
 		
 	</nav>
-	<div class="menu"></div>
+	<div class="menu">
+		<button type="button" style="float:right; margin-right:-20px;margin-top: 20px;" class="btn btn-primary bmd-btn-fab bmd-btn-fab-sm waves-effect waves-light">
+    <i class="material-icons">menu</i>
+  </button>
+	</div>
 
 </body>
 </html>
