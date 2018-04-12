04230320b59e645dcc04e582fd93f00bfc55cdc1
CarlosCampos97 <31112649+CarlosCampos97@users.noreply.github.com>
2018-04-11 23:45:01 -0600

    .o.

---
+++ res/clases/TipoCandidato.php
@@ -0,0 +1,37 @@
+<?php 
+
+/**
+* Nombre de la clase: TipoCandidato
+*/
+class TipoCandidato
+{
+	
+	private $idTipoCandidato;
+	private $tipoCandidato;
+
+	function __construct()
+	{
+		
+	}
+
+	#Metodo Get del id de candidato
+
+	public function getIdTipoCandidato()
+	{
+		return $this->idTipoCandidato;
+	}
+
+	#metodos get y set del tipo de candidato
+	public function getTipoCandidato()
+	{
+		return $this->tipoCandidato;
+	}
+
+	public function setTipoCandidato($tipoCandidato)
+	{
+		$this->tipoCandidato=$tipoCandidato;
+	}
+}
+
+
+ ?>
