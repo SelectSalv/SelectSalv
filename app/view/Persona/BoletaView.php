f592994414f0178fee3b20050c44516a0513909f
Jorge Sidgo-Pimentel <jsidgopimentel21@outlook.com>
2018-05-22 17:32:31 -0600

    cambios de la boleta

--- app/view/Persona/BoletaView.php
+++ app/view/Persona/BoletaView.php
@@ -1,13 +1,27 @@
 <link rel="stylesheet" href="res/css/grid.css">
+<link rel="stylesheet" href="res/css/boleta.css">
+
+<script>
+	$(document).ready(function() {
+		$('#nuevasIdeas').css('background-image', 'url(res/img/partidos/nuevasIdeas.jpg)');
+		$('#arena').css('background-image', 'url(res/img/partidos/arena.jpg)');
+	});	
+</script>
+<script>
+	$(document).ready(function() {
+		var altura = $();
+	});
+</script>
 
 <div class="contenedor">
-	<div class="graficos">
-		<div class="row">
-			<div class="col-md-12">
-				<div class="tarjeta boleta">
-					
-				</div>
-			</div>
-		</div>
+	<div class="barra-titulo-boleta" style="display: flex; align-items:baseline; justify-content: space-between;">
+			<p style="display: block;" class="texto-barra-titulo-boleta">
+				Papeleta
+			</p>
+			<button style="display: block;" type="button" class="btn btn-secondary btn-raised" disabled="">Votar</button>
+	</div>
+	<hr style="margin-top:0;">
+	<div class="boleta">
+		
 	</div>
 </div>
