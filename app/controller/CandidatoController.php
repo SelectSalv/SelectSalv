f1c90a6aff074200abc18fa3193f3012b8b15166
LizaUrbina <michellurbina77@gmail.com>
2018-05-27 01:03:08 +0200

    1

--- app/controller/CandidatoController.php
+++ app/controller/CandidatoController.php
@@ -25,5 +25,115 @@ class CandidatoController extends ControladorBase
 		require_once 'app/view/Candidato/candidatoView.php';
 		require_once 'app/view/plantillas/footer.php';
 	}
- 
+	public function boletaView()
+	{
+		require_once 'app/view/plantillas/header.php';
+		require_once 'app/view/plantillas/headerBarCandidato.php';
+		require_once 'app/view/Candidato/BoletaView.php';
+		require_once 'app/view/plantillas/footer.php';
+	}
+
+	# Acciones Lógicas
+
+
+	// Método para Comprobar disponibilidad de DUI
+	public function compDui()
+	{
+		$this->model->setDui($_POST["dui"]);
+		$resultado = $this->model->compDUI();
+
+		echo $resultado;
+	}
+
+	// Método para obtener Datos de Candidato por id
+	public function getCandidato()
+	{
+		/*$datos = $_POST['idCandidato'];
+
+		$datos = json_decode($datos);
+
+		$resultado = $this->model->getUsuario($datos);
+
+		echo $resultado;*/
+	}
+
+	// Método para obtener Datos de Candidato por DUI
+	public function ingresarDui()
+	{
+		$datos = $_POST["duiVotar"];
+
+		$this->model->setDui($datos);
+		$resultado = $this->model->ingresarDui();
+
+
+		echo $resultado;
+	}
+
+	// Método para obterner Candidato en formato JSON
+	public function getJSON()
+	{
+		$datos = $this->model->getCandidatoJSON();
+		
+		echo $datos;
+	}
+
+
+	// Metodo para registrar Candidato
+	public function registrarCandidato()
+	{
+
+		$datos = $_POST["datos"];
+
+		$datos = json_decode($datos);
+
+		$this->model->setDui($datos[0]->value);
+		$this->model->setNomCandidato($datos[1]->value);
+		$this->model->setApeCandidato($datos[2]->value);
+		$this->model->setGenero($datos[3]->value);
+		$this->model->setEstadoCivil($datos[4]->value);
+		$this->model->setFechaNac($datos[5]->value);
+		$this->model->setFechaVenc($datos[6]->value);
+		$this->model->setPartdio($datos[7]->value);
+		$this->model->setTipoCandidato($datos[8]->value);
+		$resultado =  $this->model->registrarCandidato();	
+
+		echo $resultado;
+	}
+
+	// Método para editar datos de Candidato
+	public function editarCandidato()
+	{
+		$datos = $_POST["datos"];
+
+		$datos = json_decode($datos);
+
+		$this->model->setDui($datos[0]->value);
+		$this->model->setNomCandidato($datos[1]->value);
+		$this->model->setApeCandidato($datos[2]->value);
+		$this->model->setGenero($datos[3]->value);
+		$this->model->setEstadoCivil($datos[4]->value);
+		$this->model->setFechaNac($datos[5]->value);
+		$this->model->setFechaVenc($datos[6]->value);
+		$this->model->setProfesion($datos[7]->value);
+		$this->model->setIdMunicipio($datos[8]->value);
+		$this->model->setDireccion($datos[9]->value);
+
+		$id = $datos[10]->value;
+
+		$resultado = $this->model->editarCandidato($id);
+
+		echo $resultado;
+	}
+
+	// Método para eliminar Candidato
+	public function eliminarCandidato()
+	{
+		$id = $_POST["idCandidato"];
+
+		$resultado = $this->model->eliminarCandidato($id);
+
+		echo $resultado;
+	}
+
 }
+ 
--- app/view/Candidato/CandidatoView.php
+++ app/view/Candidato/CandidatoView.php
@@ -4,42 +4,44 @@
   <script src="res/plugins/dataTableBootstrap/jquery.dataTables.min.js"></script>
   <script src="res/plugins/dataTableBootstrap/dataTables.bootstrap.min.js"></script>
 
-  <script src="res/ajax/PersonaAjax.js"></script>
+  <script src="res/ajax/CandidatoAjax.js"></script>
   
   <!-- modal principal de Registro -->
-  <div id="contenedor-modal">
-    <div class="modal fade" id="modalFrmPersona" tabindex="-1" role="dialog" aria-labelledby="ModalFrm" aria-hidden="true">
+ <div id="contenedor-modal">
+    <div class="modal fade" id="modalFrmCandidato" tabindex="-1" role="dialog" aria-labelledby="ModalFrm" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
           <div class="modal-header" id="modal-header-frm">
             <h5 class="modal-title" id="modal-title-frm">Registrar Candidato</h5>
           </div>    
           <div id="modal-body-frm" class="modal-body">
-              <form id="frmCandidato">
-<div class="form-row">
+              <form id="frmCandidato" >
+              <div class="form-row">
                 <div class="form-column col-md-12">
                   <div class="form-group bmd-form-group">
-              <label for="duiCandidato" class="bmd-label-floating">DUI del Candidato</label>
-              <input type="text" class="form-control dui  requeridoRegistrar" name="dui" id="dui">
+                    <label for="dui" id="label-dui" class="bmd-label-floating">DUI de el Candidato</label>
+                    <input type="text" class="form-control dui  requeridoRegistrar" name="dui" id="dui">
                     <span id="ayudaDui" class="bmd-help">El guión será agregado automáticamente</span>
                     <div id="mensajeDui" class="invalid-feedback">Ya se registró este N° de DUI</div>
                   </div>
                 </div>
               </div>
+
               <div class="form-row">
                 <div class="form-column col-md-6">
                   <div class="form-group bmd-form-group">
-                    <label for="nomPersona" class="bmd-label-floating">Nombres</label>
-                    <input type="text" class="form-control requeridoRegistrar" name="nomPersona" id="nomPersona">
+                    <label for="nomCandidato" class="bmd-label-floating">Nombres</label>
+                    <input type="text" class="form-control requeridoRegistrar" name="nomCandidato" id="nomCandidato">
                   </div>
                 </div>
                 <div class="form-column col-md-6">
                   <div class="form-group bmd-form-group">
-                    <label for="apellidosPersona" class="bmd-label-floating">Apellidos</label>
-                    <input type="text" class="form-control requeridoRegistrar" name="apellidosPersona" id="apellidosPersona">
+                    <label for="apellidosCandidato" class="bmd-label-floating">Apellidos</label>
+                    <input type="text" class="form-control requeridoRegistrar" name="apellidosCandidato" id="apellidosCandidato">
                   </div>
                 </div>
               </div>
+
               <div class="form-row">
                 <div class="form-column col-md-6">
                   <div class="form-group bmd-form-group is-filled">
@@ -79,8 +81,8 @@
                   </div>
                 </div>
               </div>
-
-        <div class="form-row">
+              
+              <div class="form-row">
           <div class="form-column col-md-6">
             <div class="form-group bmd-form-group is-filled">
               <label for="departamento" class="bmd-label-floating">Partido Político</label>
@@ -104,22 +106,12 @@
           </div>
         </div>
 
-        <br>
-        <div class="form-row">
-          <div class="form-column col-md-12">
-            <div class="form-group">
-              <div class="custom-file">
-                <input type="file" class="custom-file-input" id="customFileLang" lang="es">
-                <label class="custom-file-label" for="customFileLang">Seleccionar Foto</label>
-              </div>
-            </div>
-          </div>
-        </div>
+              
           </form>
           </div>
           <div class="modal-footer" id="modal-footer-frm">
             <button type="button" id="btnCancelarFrm" class="btn btn-secondary material-ripple" data-dismiss="modal">Cancelar</button>
-            <button type="button" id="btnFrmRegistrar" class="btn btn-success material-ripple">Registrar</button>
+            <button type="button" id="btnFrmRegistrar" class="btn btn-success material-ripple">Registrar Candidato</button>
           </div>
         </div>
       </div>
@@ -128,7 +120,7 @@
 
   <!-- modal secundario -->
   <div id="contenedor-modal">
-    <div class="modal fade" id="modalDatos" tabindex="-1" role="dialog" aria-labelledby="modalDatosPersona" aria-hidden="true">
+    <div class="modal fade" id="modalDatos" tabindex="-1" role="dialog" aria-labelledby="modalDatosCandidato" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
           <div class="modal-header" id="modal-header-datos">
@@ -170,43 +162,88 @@
               <div class="form-row">
                 <div class="form-column col-md-12">
                   <div class="form-group bmd-form-group">
-                    <label for="dui" id="label-dui" class="bmd-label-floating">DUI del Candidato</label>
-             <input type="text" class="form-control dui  requerido" name="dui" id="duiModificar">
+                    <label for="dui" id="label-dui" class="bmd-label-floating">DUI</label>
+                    <input type="text" class="form-control dui  requerido" name="dui" id="duiModificar">
                     <span id="ayudaDui" class="bmd-help">El guión será agregado automáticamente</span>
                     <div id="mensajeDui" class="invalid-feedback">Ya se registró este N° de DUI</div>
-                  </div>
-                </div>
-              </div>
-        <div class="form-row">
-                <div class="form-column col-md-6">
-                  <div class="form-group bmd-form-group">
-                   <label for="departamento" class="bmd-label-floating">Partido Político</label>
-              <select type="text" class="form-control" name="departamento" id="departamento">
-                <option value="-">Seleccione uno...</option>
-                <option value="Partido 1">Partido 1</option>
-                <option value="Partido 2">Partido 2</option>
-                <option value="Partido 3">Partido 3</option>      
-              </select>
-                  </div>
-                </div>
-                <div class="form-column col-md-6">
-                  <div class="form-group bmd-form-group">
-                    <label for="municipio" class="bmd-label-floating">Tipo de Candidato</label>
-              <select type="text" class="form-control" name="municipio" id="municipio">
-                <option value="-">Seleccione uno...</option>
-                <option value="Presidente">Presidente</option>
-                <option value="Vicepresidente">Vicepresidente</option>
-              </select>
                   </div>
                 </div>
               </div>
 
               <div class="form-row">
                 <div class="form-column col-md-6">
-                  <input type="file" class="custom-file-input" id="customFileLang" lang="es">
-                <label class="custom-file-label" for="customFileLang">Seleccionar Foto</label>
-                </div>
+                  <div class="form-group bmd-form-group">
+                    <label for="nomCandidato" class="bmd-label-floating">Nombres</label>
+                    <input type="text" class="form-control requerido" name="nomCandidato" id="nomCandidatoModificar">
                   </div>
+                </div>
+                <div class="form-column col-md-6">
+                  <div class="form-group bmd-form-group">
+                    <label for="apellidosCandidato" class="bmd-label-floating">Apellidos</label>
+                    <input type="text" class="form-control requerido" name="apellidosCandidato" id="apellidosCandidatoModificar">
+                  </div>
+                </div>
+              </div>
+
+              <div class="form-row">
+                <div class="form-column col-md-6">
+                  <label for="">Género</label>
+                  <select name="generoModificar" id="generoModificar" class="form-control">
+                    <option value="-">Seleccione uno...</option>
+                    <option value="1">Masculino</option>
+                    <option value="2">Femenino</option>
+                  </select>
+                </div>
+                  <div class="form-column col-md-6">
+                  <label for="">Estado Civil</label>
+                    <select name="estadoCivilModificar" id="estadoCivilModificar" class="form-control">
+                    <option value="-">Seleccione uno...</option>
+                    <option value="1">Soltero</option>
+                    <option value="2">Casado</option>
+                    <option value="3">Divorciado</option>
+                    <option value="4">Viudo</option>
+                  </select>
+                </div>
+              </div>
+
+              <div class="form-row">
+                <div class="form-column col-md-6">
+                  <div class="form-group bmd-form-group is-filled">
+                    <label for="fechaNacimiento" class="bmd-label-floating">Fecha de Nacimiento</label>
+                    <input type="date" class="form-control requerido" name="fechaNacimiento" id="fechaNacimientoModificar">
+                  </div>
+                </div>
+                <div class="form-column col-md-6">
+                  <div class="form-group bmd-form-group is-filled">
+                    <label for="fechaVencimiento" class="bmd-label-floating">Fecha de Vencimiento</label>
+                    <input type="date" class="form-control requerido" name="fechaVencimiento" id="fechaVencimientoModificar">
+                  </div>
+                </div>
+              </div>
+              
+              <div class="form-row">
+          <div class="form-column col-md-6">
+            <div class="form-group bmd-form-group is-filled">
+              <label for="departamento" class="bmd-label-floating">Partido Político</label>
+              <select type="text" class="form-control" name="departamento" id="departamento">
+                <option value="-">Seleccione uno...</option>
+                <option value="Partido 1">Partido 1</option>
+                <option value="Partido 2">Partido 2</option>
+                <option value="Partido 3">Partido 3</option>      
+              </select>
+            </div>
+          </div>
+          <div class="form-column col-md-6">
+            <div class="form-group bmd-form-group is-filled">
+              <label for="municipio" class="bmd-label-floating">Tipo de Candidato</label>
+              <select type="text" class="form-control" name="municipio" id="municipio">
+                <option value="-">Seleccione uno...</option>
+                <option value="Presidente">Presidente</option>
+                <option value="Vicepresidente">Vicepresidente</option>
+              </select>
+            </div>
+          </div>
+        </div>
           </form>
           </div>
           <div class="modal-footer" id="modal-footer-modificar">
@@ -219,11 +256,11 @@
   
 <!-- modal eliminar -->
   <div id="contenedor-modal">
-    <div class="modal fade" id="modalEliminar" tabindex="-1" role="dialog" aria-labelledby="modalEliminarPersona" aria-hidden="true">
+    <div class="modal fade" id="modalEliminar" tabindex="-1" role="dialog" aria-labelledby="modalEliminarCandidato" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
           <div class="modal-header" id="modal-header-eliminar">
-            <h5 class="modal-title" id="modal-title-eliminar">Eliminar del Padrón</h5>
+            <h5 class="modal-title" id="modal-title-eliminar">Eliminar Candidato</h5>
           </div>    
           <div id="modal-body-eliminar" class="modal-body">
             ¿Desea eliminar este registro?
@@ -235,9 +272,9 @@
         </div>
       </div>
     </div>
-<!-- modal Detalles -->
+  <!-- modal Detalles -->
   <div id="contenedor-modal">
-    <div class="modal fade" id="modalDetalles" tabindex="-1" role="dialog" aria-labelledby="modalDetallesPersona" aria-hidden="true">
+    <div class="modal fade" id="modalDetalles" tabindex="-1" role="dialog" aria-labelledby="modalDetallesCandidato" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
           <div class="modal-header" id="modal-header-detalles">
@@ -277,10 +314,13 @@
     <table id="tableCandidato" class="table table-hover table-bordered" style="width:100%; margin:auto;">
       <thead class="bg-primary">
               <tr>
-                <th class="text-light" >idCandidato</th>
+                  <th class="text-light" >idCandidato</th>
                   <th class="text-light" >DUI</th>
                   <th class="text-light" >Apellidos</th>
                   <th class="text-light" >Nombres</th>
+                  <th class="text-light" >Género</th>
+                  <th class="text-light" >Estado</th>
+                  <th class="text-light" >Fecha de Nacimiento</th>
                   <th class="text-light" >Partido</th>
                   <th class="text-light" >Tipo de Candidato</th>
               </tr>
